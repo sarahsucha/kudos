5dc7a94ec7119f2389a7f84f302f345e27f35b23
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-08-27 11:14:17 -0700

    Define react routes

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,14 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, Link, browserHistory } from 'react-router';
 
-// ReactDOM.render(
-//   <h1>Rename it somehow to make sure whatever</h1>,
-//   document.getElementById('main')
-// );
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="kudos" component={KudosListPage}/>
+    </Route>
+  </Router>
+), document.getElementById('main'))
+
+// <Route path="kudos/edit/:kudoId" component={KudosEditPage}/>
+// <Route path="*" component={NotFoundPage}/>
